1fb547505c4d18b035c230afd2dca7f5924a0170
EnPere <pereruba@gmail.com>
2021-11-01 19:01:03 +0100

    archivos a testear agregados a app

---
+++ Sprint1_testing/app/sprint1nivell1_codigotest.js
@@ -0,0 +1,24 @@
+// Nivell 1
+// Crea un arxiu amb les funcions sumar, restar, multiplicar i dividir dos o més operands. 
+// Testeja la correcta execució d'aquestes funcions.
+// Crea els tests corresponents per verificar el funcionament de l'exercici Async / Await Nivell 1 - Exercici 1
+// Crea els tests corresponents per verificar el funcionament de l'exercici Async / Await Nivell 2 - Exercici 1
+// Crea els tests corresponents per verificar el funcionament de l'exercici Promises & Callbacks Nivell 2 - Exercici 3
+// Verifica mitjançant tests l'execució de l'exercici Async / Await Nivell 2 Exercici 1 utilitzant Jest Fake Timers.
+
+
+const suma = (num1, num2) => {
+  return num1 + num2
+}
+
+const resta = (num1, num2) => {
+  return num1 - num2
+}
+
+const multiplicar = (num1, num2) => {
+  return num1 * num2
+}
+
+const dividir = (num1, num2) => {
+  return num1 / num2
+}
